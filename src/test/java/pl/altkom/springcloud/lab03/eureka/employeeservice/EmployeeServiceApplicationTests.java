0cd07ce92e594abedbd1c4716f4d966387747387
Artur Mroczkowski <a.mroczkowski@intive.com>
2022-05-23 00:24:51 +0200

    Init

---
+++ src/test/java/pl/altkom/springcloud/lab03/eureka/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.springcloud.lab03.eureka.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
